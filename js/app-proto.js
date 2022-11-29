bdb5f1dca3a4eed1f0b39ce24b26baab81adf5fc
Marco silva <marco.silvaponce10@gmail.com>
2022-11-29 01:42:19 -0300

    POO projects: Counter

---
+++ js/app-proto.js
@@ -0,0 +1,58 @@
+function getElement(selection) {
+  const element = document.querySelector(selection);
+  // console.log(element);
+  if (element) {
+    return element;
+  }
+  throw new Error(
+    `Please check "${selection}" selector, no such element exist`
+  );
+}
+
+function Counter(element, value) {
+  // console.log(element, value);
+  this.counter = element;
+  this.value = value;
+  this.resetBtn = element.querySelector(".reset");
+  this.increaseBtn = element.querySelector(".increase");
+  this.decreaseBtn = element.querySelector(".decrease");
+  this.valueDOM = element.querySelector(".value");
+  this.valueDOM.textContent = this.value;
+  // console.log(this.valueDOM);
+
+  // bind this to all function
+  this.increase = this.increase.bind(this);
+  this.decrease = this.decrease.bind(this);
+  this.reset = this.reset.bind(this);
+
+  this.increaseBtn.addEventListener("click", this.increase);
+  this.decreaseBtn.addEventListener("click", this.decrease);
+  this.resetBtn.addEventListener("click", this.reset);
+}
+
+Counter.prototype.increase = function () {
+  console.log(this);
+  this.value++;
+  this.valueDOM.textContent = this.value;
+};
+Counter.prototype.decrease = function () {
+  this.value--;
+  this.valueDOM.textContent = this.value;
+};
+Counter.prototype.reset = function () {
+  this.value = 0;
+  this.valueDOM.textContent = this.value;
+};
+
+const firstCounter = new Counter(getElement(".first-counter"), 100);
+
+const secondCounter = new Counter(getElement(".second-counter"), 200);
+
+
+// firstCounter.increase();
+// firstCounter.increase();
+// firstCounter.decrease();
+// secondCounter.increase();
+// secondCounter.decrease();
+// secondCounter.decrease();
+// secondCounter.reset();
